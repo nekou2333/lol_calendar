1d6e2d043187bf058a17054940feb55cde3dcba8
Kabigon Zhao <meowstic.zyx@gmail.com>
2025-03-17 23:31:29 +0800

    fix: 创建文件夹

--- consts.py
+++ consts.py
@@ -2,4 +2,8 @@ from pathlib import Path
 
 API_URL = "https://lpl.qq.com/web201612/data/LOL_MATCH2_MATCH_HOMEPAGE_BMATCH_LIST.js"
 OUTPUTS = Path("outputs")
+if not OUTPUTS.exists():
+    OUTPUTS.mkdir(parents=True)
 GAMES = OUTPUTS.joinpath("games")
+if not GAMES.exists():
+    GAMES.mkdir(parents=True)
